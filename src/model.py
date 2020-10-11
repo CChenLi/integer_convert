362d6949ca138585bcee70d313f2481984eb8f84
CChenLi <lichen918@g.ucla.edu>
2020-10-10 18:46:50 -0700

    add IntConversion

--- src/model.py
+++ src/model.py
@@ -1,7 +1,26 @@
+import csv
+import cv2
+import time
+import matplotlib.pyplot as plt
 import torch
-import torch.nn as nn
+from torch.utils.data import DataLoader
+import torch.optim as optim
 import torch.nn.functional as F
+from torchvision import transforms
+from utils import IntImage, get_num_correct, HelperFunc
+from PIL import Image, ImageOps
+import numpy as np
+import pickle
+from argparse import ArgumentParser
+import torch.nn as nn
 
+def load_checkpoint(path, model, optimizer):
+    checkpoint = torch.load(path, map_location='cpu')
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    loss = checkpoint['loss']
+    return epoch, loss
 
 class IntRec(nn.Module):
     def __init__(self):
@@ -37,3 +56,63 @@ class IntRec(nn.Module):
         # t = F.softmax(t, dim=1)
 
         return t
+
+class IntConversion(nn.Module):
+    def __init__(self, model, pad):
+        super(IntConversion, self).__init__()
+        self.model = model
+        self.pad = pad
+        self.kernal = np.ones((2,2),np.uint8)
+
+    def pic_preprocess(self, path):
+        pic = cv2.imread(path)
+        gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+        _, bw = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY_INV)
+        pic_tensor = torch.Tensor(bw)
+        horizontal_mask = torch.ones(pic_tensor.size(1), 1)
+        horizontal_index = torch.mm(pic_tensor, horizontal_mask)
+        slices = HelperFunc.get_slices((horizontal_index != 0).view(-1))
+        return slices, pic_tensor
+
+    def trans(self, cuts, fig, example_slice):
+        for i, cut in enumerate(cuts):
+            fig.add_subplot(1, len(cuts), i+1)
+            start_index, end_index = cut
+            buff = int(0.2 * (end_index - start_index))
+            short_pic_tensor = example_slice[: ,start_index-buff : end_index + buff]
+            # Trim the data
+            img = transforms.ToPILImage(mode='L')(short_pic_tensor)
+            p = transforms.Compose([transforms.Scale((self.pad, self.pad))])
+            new_img = ImageOps.expand(p(img),border=((64-self.pad) // 2),fill='black')
+            new_img = np.array(new_img)
+            new_img = cv2.dilate(new_img,self.kernal,iterations = 1)
+            # Prediction
+            trans = transforms.ToTensor()
+            new_tensor = torch.unsqueeze(trans(new_img), 0)
+            new_tensor[new_tensor > 0] = 255
+            pred = self.model(new_tensor).argmax()
+            plt.imshow(new_tensor.reshape(64, 64), cmap="gray")
+            plt.title(f"pred: {pred}")
+
+    def forward(self, path):
+        slices, pic_tensor = self.pic_preprocess(path)
+        for i, slice in enumerate(slices):
+            example_slice = pic_tensor[slice[0] : slice[1], : ]
+            cuts = HelperFunc.get_characters(example_slice)
+            fig = plt.figure(figsize=[2 * len(cuts), 2])
+            self.trans(cuts, fig, example_slice)
+            plt.savefig(f"processed/pic_line_{i}.png")
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('path', type=str, help='path to the picture')
+    path = args = parser.parse_args().path
+    print(path)
+    model = IntRec()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    load_checkpoint("saved_model/model1.pickle", model, optimizer)
+    converter = IntConversion(model, 28)
+    converter(path)
+
+if __name__=="__main__":
+    main()
